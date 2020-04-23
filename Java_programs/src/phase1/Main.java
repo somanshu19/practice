f16aa97403ca4d2caaf543a3e902f2343f7b6426
Kumar <AG22126@ad.wellpoint.com>
2020-04-24 00:15:09 +0530

    updating main.java

--- Java_programs/src/phase1/Main.java
+++ Java_programs/src/phase1/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Helooooooooooooooooo");
+		System.out.println("testing.....");
 
 	}
 
