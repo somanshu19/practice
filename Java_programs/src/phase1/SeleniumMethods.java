7e63c51f0a20cbaa5262d33da4915e0878dcea06
Somanshu <somanshukumar@gmail.com>
2020-05-01 23:59:19 +0530

    Updating the java file - SeleniumMethods.java

--- Java_programs/src/phase1/SeleniumMethods.java
+++ Java_programs/src/phase1/SeleniumMethods.java
@@ -1,18 +1,21 @@
 package phase1;
 
-import java.util.concurrent.TimeUnit;
-
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class SeleniumMethods {
 	
+	static String driverPath = System.setProperty("webdriver.chrome.driver", "\\Driver\\chromedriver.exe");
+	protected static WebDriver driver = new ChromeDriver();
+	
 	public static void LaunchURL(String url){
 		
-		System.setProperty("webdriver.chrome.driver", "\\Driver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
 		try{
-			driver.manage().timeouts().implicitlyWait(3,TimeUnit.SECONDS);
+			Thread.sleep(2000);
 			driver.get(url);
 			driver.manage().window().maximize();
 			
@@ -28,12 +31,79 @@ public class SeleniumMethods {
 		catch(Exception e){
 			System.out.println("Exception is :" + e);
 		}
-		driver.quit();
 	}
+	
+	public static void MouseClick(String xPath){
+		
+		try{
+			Thread.sleep(2000);
+			WebElement WElement = driver.findElement(By.xpath(xPath));
+			
+			if(WElement.isDisplayed() && WElement.isEnabled()){
+				WElement.click();
+				System.out.println("Clicked on WElement");
+			}
+			else{
+				System.out.println("Web element is not displayed");
+			}
+		}
+		catch(Exception e){
+			System.out.println("Exception is :" + e);
+		}		
+	}
+	
+	public static void KeysEnter_XPATH(String xPath, String textValue){
+		
+		try{
+			Thread.sleep(2000);
+			WebElement WElement = driver.findElement(By.xpath(xPath));
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		LaunchURL("https://www.amazon.in/");
+			if(WElement.isDisplayed() && WElement.isEnabled()){
+				WElement.sendKeys(textValue);
+				
+				String text = WElement.getAttribute("value");
+				
+				if(text.equals(textValue)){
+					System.out.println("Text entered successfully");
+				}
+				else{
+					System.out.println("Error occured while entering text");
+				}
+			}
+			else{
+				System.out.println("Web element is not displayed & is not enabled");
+			}
+		}
+		catch(Exception e){
+			System.out.println("Exception is :" + e);
+		}		
+	}
+	
+	public static void JSKeysEnter_ID(String iD, String textValue){
+		
+		try{
+			Thread.sleep(2000);
+			WebElement WElement = driver.findElement(By.id(iD));
+			
+			if(WElement.isDisplayed() && WElement.isEnabled()){
+				WElement.sendKeys(textValue);
+				
+				JavascriptExecutor e = (JavascriptExecutor) driver;
+				String text = (String) e.executeScript("return document.getElementById('" + iD +"').value");
+				
+				if(text.equals(textValue)){
+					System.out.println("Text entered successfully");
+				}
+				else{
+					System.out.println("Error occured while entering text");
+				}
+			}
+			else{
+				System.out.println("Web element is not displayed & is not enabled");
+			}			
+		}
+		catch(Exception e){
+			System.out.println("Exception is :" + e);
+		}		
 	}
 }
